8b0fb8cc6c251896d68a18d0d35e55b70dfec937
Grooty12 <Rasmus@Rosendahl-kaa.name>
2022-10-04 09:45:18 +0200

    Commit

--- Kap 5/Ex 11/sketch.js
+++ Kap 5/Ex 11/sketch.js
@@ -1,7 +1,21 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function draw() {
-  background(220);
+  createCanvas(800, 800);
+  for (let j = 0; j < 800; j += 100) {
+    for (let i = 0; i < 800; i += 100) {
+      if ((i / 100) % 2 == 0) {
+        if ((j / 100) % 2 == 0) {
+          fill(255);
+        } else {
+          fill(0);
+        }
+      } else {
+        if ((j / 100) % 2 == 1) {
+          fill(255);
+        } else {
+          fill(0);
+        }
+      }
+      rect(i, j, 100, 100);
+    }
+  }
 }

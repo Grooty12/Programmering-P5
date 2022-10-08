d8a1389d8c54fa65b52d52d13813d03e43457d9d
Rasmus Rosendahl-Kaa <github.q2pcy@simplelogin.com>
2022-10-08 23:32:48 +0200

    Chess

--- Funny Projects/Chess/sketch.js
+++ Funny Projects/Chess/sketch.js
@@ -1,8 +1,20 @@
+
+let square = [];
+let a = "A"
+let b = "B"
+let c = "C"
+let d = "D"
+let e = "E"
+let f = "F"
+let g = "G"
+let h = "H"
 let n = 8;
 let size = 800;
 function setup() {
   createCanvas(size, size);
   let P = size / n;
+  let square1;
+  let square2 = [];
   for (let j = 0; j * P < height; j++) {
     for (let i = 0; i * P < width; i++) {
       if (i % 2 == 0) {
@@ -18,7 +30,26 @@ function setup() {
           fill(0);
         }
       }
-      rect(i * P, j * P, P, P);
+      square2[i] = [j * P + (P / 2), i * P + (P / 2)];
+      rect(j * P, i * P, P, P);
     }
+    square1 = char(j + 65);
+    print("j * P + (P / 2) =", j * P + (P / 2))
+    square[square1] = square2;
+    print(square2)
   }
+
+  imageMode(CENTER);
+  print("square[a][1] = ", square["A"][1])
+  print("square = ", square)
 }
+/*
+function draw() {
+  DrawBricks("pawn", square[h][3])
+}
+
+function DrawBricks(type, place) {
+  if (type == "pawn") {
+    circle(place[0], place[1], 50);
+  }
+}*/

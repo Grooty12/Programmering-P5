92026f4cddedc6a50970043646595c4e869f5c70
Grooty12 <Rasmus@Rosendahl-kaa.name>
2022-10-08 21:27:40 +0200

    Chess

---
+++ Funny Projects/Chess/sketch.js
@@ -0,0 +1,24 @@
+let n = 8;
+let size = 800;
+function setup() {
+  createCanvas(size, size);
+  let P = size / n;
+  for (let j = 0; j * P < height; j++) {
+    for (let i = 0; i * P < width; i++) {
+      if (i % 2 == 0) {
+        if (j % 2 == 0) {
+          fill(255);
+        } else {
+          fill(0);
+        }
+      } else {
+        if (j % 2 == 1) {
+          fill(255);
+        } else {
+          fill(0);
+        }
+      }
+      rect(i * P, j * P, P, P);
+    }
+  }
+}

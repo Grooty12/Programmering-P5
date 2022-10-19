e626183d02c7ca9dc6848939db63c32087b60f0f
Rasmus Rosendahl-Kaa <github.q2pcy@simplelogin.com>
2022-10-19 23:33:32 +0200

    Chess

--- Funny Projects/Chess/sketch.js
+++ Funny Projects/Chess/sketch.js
@@ -13,43 +13,53 @@ function preload() {
 function setup() {
   createCanvas(size, size);
   P = size / n;
-  textAlign(CENTER);
-  imageMode(CENTER);
+  textAlign(CENTER); // Gør så tekst skrives i midten af coords
+  imageMode(CENTER); // Samme bare med billeder
   noFill;
-  bricks[0] = ["pawn", 3, 3];
+  bricks[0] = ["pawn", 3, 3]; // Test, definerer brick 0 til en pawn.
   fill(255);
 }
 function draw() {
   for (let j = 0; j * P < height; j++) {
-    let row1 = [];
-    let color;
+    // For loop. Hvor mange felter man er nede. y-værdi
+    let row1 = []; // Row1 er rækkens felters coords. Laves til blank ved start af hver j-loop, som er antal række nede
+    let color; // Color er firkant farve. Defineres da teksten skal være modsat farve.
     for (let i = 0; i * P < width; i++) {
+      // For loop igen. Er hvor mange felter man er henne på rækken. x-værdi
       if (i % 2 == 0) {
+        // Hvis i (feltes x-værdi) er lige
         if (j % 2 == 0) {
+          // Hvis j (y-værdi) er lige
           color = 255;
         } else {
+          // Hvis j er ulige
           color = 0;
         }
       } else {
+        // Hvis i er ulige
         if (j % 2 == 1) {
+          // Hvis j er ulige
           color = 255;
         } else {
+          // Hvis j er lige
           color = 0;
         }
       }
       if (DefineSquares == 0) {
+        // I første "frame" defineres felteres x-værdi
         row1.push([j * P + 50, i * P + 50, j + i * n + 1]);
       }
       fill(color);
-      rect(j * P, i * P, P, P);
+      rect(j * P, i * P, P, P); // tegner firkanterne
       fill(255 - color);
-      text(j + i * n + 1, j * P + 50, i * P + 50);
+      text(j + i * n + 1, j * P + 50, i * P + 50); // Skriver felt nummer
     }
     if (DefineSquares == 0) {
+      // I første "frame" defineres alle felteres x-værdi per felternes y-værdi
       square.push(row1);
     }
   }
-  DefineSquares = 1;
+  DefineSquares = 1; // Til de ting, som kun skal køre 1 gang, spares for-loops i function setup
   DrawBricks(bricks[0]);
 }
 

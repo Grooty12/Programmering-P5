e02bba1354b83873c2316d45d3d39542941fcbe7
Rasmus Rosendahl-Kaa <github.q2pcy@simplelogin.com>
2022-10-20 16:04:22 +0200

    Chess

--- Funny Projects/Chess/sketch.js
+++ Funny Projects/Chess/sketch.js
@@ -1,14 +1,38 @@
 let square = []; // Det er koordinater til hver firkant.
-let squares = [];
+let brick = []; // Definerer brikkerne, deres type og deres koordinater
 let n = 8; // Antal firkanter
 let size = 800;
 let P; // Hvor langt der skal være mellem hver firkant
-let bricks = []; // Definerer brikkerne, deres type og deres koordinater
 let pawnIMG; // Bonde IMG
-let DefineSquares = 0; // Gør så Firkanterne kun bliver defineret 1 gang i draw funktionen
+let pawnIMGR = [180 / 3, 247 / 3];
+let IMG = [
+  [0, 0, 255 / 3, 277 / 3],
+  [0, 0, 255 / 3, 277 / 3],
+  [0, 0, 291 / 3, 272 / 3],
+  [0, 0, 212 / 3, 247 / 3],
+  [0, 0, 271 / 3, 291 / 3],
+  [0, 0, 252 / 3, 249 / 3],
+  [0, 0, 180 / 3, 247 / 3],
+];
+let DefineSquares = 0; // Gør så Firkanterne kun bliver defineret 1 gang'
+let BrickPressed = [];
+let PawnAttack = 0;
 
 function preload() {
-  pawnIMG = loadImage("assets/White/Pawn.png");
+  IMG[0][2] = loadImage("assets/Blank.png");
+  IMG[0][1] = loadImage("assets/Blank.png");
+  IMG[1][0] = loadImage("assets/White/King.png");
+  IMG[1][1] = loadImage("assets/Black/King.png");
+  IMG[2][0] = loadImage("assets/White/Queen.png");
+  IMG[2][1] = loadImage("assets/Black/Queen.png");
+  IMG[3][0] = loadImage("assets/White/Rook.png");
+  IMG[3][1] = loadImage("assets/Black/Rook.png");
+  IMG[4][0] = loadImage("assets/White/Bishop.png");
+  IMG[4][1] = loadImage("assets/Black/Bishop.png");
+  IMG[5][0] = loadImage("assets/White/Knight.png");
+  IMG[5][1] = loadImage("assets/Black/Knight.png");
+  IMG[6][0] = loadImage("assets/White/Pawn.png");
+  IMG[6][1] = loadImage("assets/Black/Pawn.png");
 }
 
 function setup() {
@@ -17,10 +41,58 @@ function setup() {
   textAlign(CENTER); // Gør så tekst skrives i midten af coords
   imageMode(CENTER); // Samme bare med billeder
   noFill;
-  bricks[0] = ["pawn", 3, 3]; // Test, definerer brick 0 til en pawn.
   fill(255);
+  DrawSquares();
+  DefineSquares = 1; // Til de ting, som kun skal køre 1 gang
+  DefineBricks();
+  DrawBricks(99);
 }
-function draw() {
+function DefineBricks() {
+  // King
+  brick[4][0] = 1;
+  brick[4][1] = 1;
+  brick[59][0] = 1;
+  brick[59][1] = 0;
+  // Queen
+  brick[3][0] = 2;
+  brick[3][1] = 1;
+  brick[60][0] = 2;
+  brick[60][1] = 0;
+  for (let i = 0; i < 2; i++) {
+    // Rook, Knight and Bishop
+    // Rook
+    let number = i * 7 + 7 * n;
+    brick[number][0] = 3;
+    brick[number][1] = 0;
+    number = i * 7 + 0 * n;
+    brick[number][0] = 3;
+    brick[number][1] = 1;
+    // Bishop
+    number = i * 3 + 0 * n + 2;
+    brick[number][0] = 4;
+    brick[number][1] = 1;
+    number = i * 3 + 7 * n + 2;
+    brick[number][0] = 4;
+    brick[number][1] = 0;
+    // Knight
+    number = i * 5 + 0 * n + 1;
+    brick[number][0] = 5;
+    brick[number][1] = 1;
+    number = i * 5 + 7 * n + 1;
+    brick[number][0] = 5;
+    brick[number][1] = 0;
+  }
+  for (let i = 0; i < 8; i++) {
+    // Pawns
+    let number = i + 6 * n;
+    brick[number][0] = 6;
+    brick[number][1] = 0;
+    number = i + 1 * n;
+    brick[number][0] = 6;
+    brick[number][1] = 1;
+  }
+}
+function DrawSquares() {
   for (let j = 0; j * P < height; j++) {
     // For loop. Hvor mange felter man er nede. y-værdi
     let row1 = []; // Row1 er rækkens felters coords. Laves til blank ved start af hver j-loop, som er antal række nede
@@ -31,80 +103,68 @@ function draw() {
         // Hvis i (feltes x-værdi) er lige
         if (j % 2 == 0) {
           // Hvis j (y-værdi) er lige
-          color = 255;
+          fill(255);
         } else {
           // Hvis j er ulige
-          color = 0;
+          fill(43, 19, 14);
         }
       } else {
         // Hvis i er ulige
         if (j % 2 == 1) {
           // Hvis j er ulige
-          color = 255;
+          fill(255);
         } else {
           // Hvis j er lige
-          color = 0;
+          fill(43, 19, 14);
         }
       }
       if (DefineSquares == 0) {
         // I første "frame" defineres felteres x-værdi
         row1.push([j * P + 50, i * P + 50, j + i * n + 1]);
-        squares.push([0, j * P + 50, i * P + 50]);
-        print(i * P);
+        brick.push([0, 2, i * P + 50, j * P + 50]);
       }
-      fill(color);
       rect(j * P, i * P, P, P); // tegner firkanterne
-      fill(255 - color);
-      text(j + i * n + 1, j * P + 50, i * P + 50); // Skriver felt nummer
     }
     if (DefineSquares == 0) {
       // I første "frame" defineres alle felteres x-værdi per felternes y-værdi
       square.push(row1);
     }
   }
-  if (DefineSquares == 0) {
-    for (let i = 0; i < 8; i++) {
-      let number = 6 + i * n;
-      squares[number][0] = ["Pawn"];
-    }
-  }
-  DefineSquares = 1; // Til de ting, som kun skal køre 1 gang, spares for-loops i function setup
-  DrawBricks(bricks[0]);
 }
 
-function DrawBricks(brick) {
-  for (let i = 0; i < 64; i++) {
-    if (squares[i][0] == "Pawn") {
-      fill(120);
-      circle(squares[i][1], squares[i][2], 50);
-    }
+function DrawBricks(x) {
+  DrawSquares();
+  fill(170, 255, 0);
+  if (x < 65) {
+    rect(brick[x][2] - 50, brick[x][3] - 50, 100, 100);
   }
-  if (brick[0] == "pawn") {
+  for (let i = 0; i < 64; i++) {
     image(
-      pawnIMG,
-      square[brick[1]][brick[2]][0],
-      square[brick[1]][brick[2]][1]
+      IMG[brick[i][0]][brick[i][1]],
+      brick[i][2],
+      brick[i][3],
+      IMG[brick[i][0]][2],
+      IMG[brick[i][0]][3]
     );
   }
 }
 
 function mousePressed() {
-  for (let i = 0; i * P < height; i++) {
-    // For-loop. Hvor mange felter man er nede
-    for (let j = 0; j * P < width; j++) {
-      // For loop. Hvor mange felter man er henne
-      if (
-        mouseX > square[i][j][0] - P / 2 && // Tjekker for hver firkants x-værdi for at se hvilken firkant musen er inden for
-        mouseX < square[i][j][0] + P / 2
-      ) {
-        if (
-          mouseY > square[i][j][1] - P / 2 && // Gør Det samme bare nedad med y-værdier
-          mouseY < square[i][j][1] + P / 2
-        ) {
-          print(square[i][j][2], i, j);
-          if (i == bricks[0][1] && j == bricks[0][2]) {
-            bricks[0][1] += 2;
-          }
+  for (let i = 0; i < 64; i++) {
+    if (mouseX > brick[i][2] - 50 && mouseX < brick[i][2] + 50) {
+      if (mouseY > brick[i][3] - 50 && mouseY < brick[i][3] + 50) {
+        if (BrickPressed[0] == 1) {
+          BrickIsPressed(
+            brick[BrickPressed[1]],
+            brick[BrickPressed[1]][0],
+            brick[BrickPressed[1]][1],
+            brick[i],
+            BrickPressed[1]
+          );
+          break;
+        } else if (brick[i][0] > 0) {
+          BrickPressed = [1, i];
+          DrawBricks(BrickPressed[1]);
           break;
         }
       }
@@ -112,4 +172,150 @@ function mousePressed() {
   }
 }
 
-function CheckPressBrick(line, row) {}
+function BrickIsPressed(Bricks, type, color, pressed, i) {
+  if (pressed[2] == Bricks[2] && pressed[3] == Bricks[3]) {
+    BrickPressed = [];
+    DrawBricks();
+  } else if (CheckKills(Bricks, pressed, type, i)) {
+    if (type == 1) {
+      // King
+      if (
+        pressed[2] == Bricks[2] + 100 ||
+        pressed[2] == Bricks[2] - 100 ||
+        (pressed[2] == Bricks[2] && pressed[3] != Bricks[3])
+      ) {
+        if (
+          pressed[3] == Bricks[3] + 100 ||
+          pressed[3] == Bricks[3] - 100 ||
+          (pressed[3] == Bricks[3] && pressed[2] != Bricks[3])
+        ) {
+          BrickIsPressed2(Bricks, pressed);
+        }
+      }
+    } else if (type == 2) {
+      // Queen
+      for (let i = 1; i <= 8; i++) {
+        if (
+          pressed[2] == Bricks[2] + i * 100 ||
+          pressed[2] == Bricks[2] - i * 100 ||
+          (pressed[2] == Bricks[2] && pressed[3] != Bricks[3])
+        ) {
+          if (
+            pressed[3] == Bricks[3] + i * 100 ||
+            pressed[3] == Bricks[3] - i * 100 ||
+            (pressed[3] == Bricks[3] && pressed[2] != Bricks[3])
+          ) {
+            BrickIsPressed2(Bricks, pressed);
+          }
+        }
+      }
+    } else if (type == 3) {
+      // Rook
+      if (pressed[2] == Bricks[2]) {
+        if (
+          (pressed[3] >= Bricks[3] + 100 && pressed[3] <= Bricks[3] + 800) ||
+          (pressed[3] <= Bricks[3] - 100 && pressed[3] >= Bricks[3] - 800)
+        ) {
+          BrickIsPressed2(Bricks, pressed);
+        }
+      } else if (
+        pressed[2] >= Bricks[2] + 100 &&
+        pressed[2] <= Bricks[2] + 800 &&
+        pressed[2] <= Bricks[2] - 100 &&
+        pressed[2] >= Bricks[2] - 800
+      ) {
+        if (pressed[3] == Bricks[3]) {
+          BrickIsPressed2(Bricks, pressed);
+        }
+      }
+    } else if (type == 4) {
+      // Bishop
+      for (let i = 1; i <= 8; i++) {
+        if (
+          pressed[2] == Bricks[2] + i * 100 ||
+          pressed[2] == Bricks[2] - i * 100
+        ) {
+          if (
+            pressed[3] == Bricks[3] + i * 100 ||
+            pressed[3] == Bricks[3] - i * 100
+          ) {
+            BrickIsPressed2(Bricks, pressed);
+          }
+        }
+      }
+    } else if (type == 5) {
+      // Knight
+      if (pressed[2] == Bricks[2] - 100 || pressed[2] == Bricks[2] + 100) {
+        if (pressed[3] == Bricks[3] + 200 || pressed[3] == Bricks[3] - 200) {
+          BrickIsPressed2(Bricks, pressed);
+        }
+      } else if (
+        pressed[2] == Bricks[2] - 200 ||
+        pressed[2] == Bricks[2] + 200
+      ) {
+        if (pressed[3] == Bricks[3] + 100 || pressed[3] == Bricks[3] - 100) {
+          BrickIsPressed2(Bricks, pressed);
+        }
+      }
+    } else if (type == 6) {
+      // Pawn
+      if (pressed[2] == Bricks[2]) {
+        if (
+          (pressed[3] >= Bricks[3] + 100 &&
+            pressed[3] <= Bricks[3] + 200 &&
+            color == 1) ||
+          (pressed[3] <= Bricks[3] - 100 &&
+            pressed[3] >= Bricks[3] - 200 &&
+            color == 0)
+        ) {
+          BrickIsPressed2(Bricks, pressed);
+        }
+      }
+    }
+  }
+  if (PawnAttack == 1) {
+    if (pressed[2] == Bricks[2] + 100 || pressed[2] == Bricks[2] - 100) {
+      if (
+        (pressed[3] == Bricks[3] + 100 && color == 1) ||
+        (pressed[3] == Bricks[3] - 100 && color == 0)
+      ) {
+        BrickIsPressed2(Bricks, pressed);
+      }
+    }
+  }
+}
+
+function BrickIsPressed2(Bricks, pressed) {
+  pressed[0] = Bricks[0];
+  pressed[1] = Bricks[1];
+  Bricks[0] = 0;
+  Bricks[1] = 2;
+  BrickPressed = [];
+  DrawBricks();
+}
+
+function CheckKills(Bricks, pressed, type, i) {
+  if (Bricks[1] == pressed[1]) {
+    print("False");
+    return false;
+  } else if (type == 6) {
+    if (
+      ((brick[i - 7][1] == 1 || brick[i - 9][1] == 1) && Bricks[1] == 0) ||
+      ((brick[i + 7][1] == 0 || brick[i + 9][1] == 0) && Bricks[1] == 1)
+    ) {
+      PawnAttack = 1;
+      print(PawnAttack);
+    } else {
+      PawnAttack = 0;
+      print(PawnAttack);
+    }
+    if (
+      (brick[i - 8][0] != 0 && Bricks[1] == 0) ||
+      (brick[i + 8][0] != 0 && Bricks[1] == 1)
+    ) {
+      print("hmm");
+      return false;
+    }
+  }
+  return true;
+}

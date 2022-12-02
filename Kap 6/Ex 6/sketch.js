489ceac5f2ea025313e6d6f0d11e7ad84b3224da
Rasmus Rosendahl-Kaa <github.q2pcy@simplelogin.com>
2022-12-02 14:12:53 +0100

    kapt 6

--- Kap 6/Ex 6/sketch.js
+++ Kap 6/Ex 6/sketch.js
@@ -1,7 +1,8 @@
-let input1, input2, input3, button;
+let input1, input2, input3, button, text, resetbutton;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(250);
+  background(100);
+  textAlign(CENTER);
   input1 = createInput();
   input1.position(200, 100);
 
@@ -11,18 +12,30 @@ function setup() {
   input3 = createInput();
   input3.position(input1.x, input2.y + input2.height + 5);
 
-  button = createButton("submit");
+  button = createButton("Create");
   button.position(input1.x + input1.width, 100);
   button.mousePressed(create);
+
+  resetbutton = createButton("Reset");
+  resetbutton.position(button.x, button.y);
+  resetbutton.mousePressed(reset);
+  resetbutton.hide();
+
+  fill(10);
+  title = createElement(
+    "h3",
+    "Create a triangle and define the lenth of the sides (value is times 10)"
+  );
+  title.position(200, 50);
   angleMode(DEGREES);
 }
 
 function create() {
-  let a = int(input1.value());
-  let b = int(input2.value());
-  let c = int(input3.value());
+  let a = int(input1.value()) * 10;
+  let b = int(input2.value()) * 10;
+  let c = int(input3.value()) * 10;
   if (!(a + b > c && a + c > b && b + c > a)) {
-    print("NOT A TRIANGLE");
+    title.html("NOT A TRIANGLE. TRY AGAIN");
     return false;
   }
 
@@ -30,6 +43,13 @@ function create() {
 
   let height = sin(CV) * b;
   let a2 = cos(CV) * b;
-  triangle(500, 300, 500 + a, 300, 500 + a2, 300 - height);
-  print(500, 300, 500 + a, 300, 500 + a2, 300 - height);
+  triangle(500, 500, 500 + a, 500, 500 + a2, 500 - height);
+  title.html("Done");
+  button.hide();
+  resetbutton.show();
+}
+
+function reset() {
+  title.html("");
+  setup();
 }
--- Kap 6/Ex 7/sketch.js
+++ Kap 6/Ex 7/sketch.js
@@ -4,4 +4,9 @@ function setup() {
 
 function draw() {
   background(220);
+  line(100, 600, 300, 600);
+  line(200, 100, 200, 600);
+  line(200, 100, 600, 100);
+  line(200, 200, 300, 100);
+  line(600, 100, 600, 200);
 }

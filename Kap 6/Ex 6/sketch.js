fd4c96eb7df977002b8619802240831c0b05b7dc
Rasmus Rosendahl-Kaa <github.q2pcy@simplelogin.com>
2022-12-02 22:30:20 +0100

    Kap 6

--- Kap 6/Ex 6/sketch.js
+++ Kap 6/Ex 6/sketch.js
@@ -1,39 +1,34 @@
-let input1, input2, input3, button, text, resetbutton;
+let input, button, text, resetbutton;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(100);
   textAlign(CENTER);
-  input1 = createInput();
-  input1.position(200, 100);
-
-  input2 = createInput();
-  input2.position(input1.x, input1.y + input1.height + 5);
-
-  input3 = createInput();
-  input3.position(input1.x, input2.y + input2.height + 5);
+  input = createInput();
+  input.position(200, 100);
 
   button = createButton("Create");
-  button.position(input1.x + input1.width, 100);
+  button.position(input.x + input.width, 100);
   button.mousePressed(create);
 
   resetbutton = createButton("Reset");
-  resetbutton.position(button.x, button.y);
+  resetbutton.position(input.x, input.y);
   resetbutton.mousePressed(reset);
   resetbutton.hide();
 
   fill(10);
   title = createElement(
     "h3",
-    "Create a triangle and define the lenth of the sides (value is times 10)"
+    "Define length of triangle sides (value is times 10. Comma seperated)"
   );
   title.position(200, 50);
   angleMode(DEGREES);
 }
 
 function create() {
-  let a = int(input1.value()) * 10;
-  let b = int(input2.value()) * 10;
-  let c = int(input3.value()) * 10;
+  let lengths = split(input.value(), ",");
+  let a = int(lengths[0]) * 10;
+  let b = int(lengths[1]) * 10;
+  let c = int(lengths[2]) * 10;
   if (!(a + b > c && a + c > b && b + c > a)) {
     title.html("NOT A TRIANGLE. TRY AGAIN");
     return false;
@@ -46,6 +41,7 @@ function create() {
   triangle(500, 500, 500 + a, 500, 500 + a2, 500 - height);
   title.html("Done");
   button.hide();
+  input.hide();
   resetbutton.show();
 }
 

f2d16eaa5636243a8d05358eaafdbfa758309352
Rasmus Rosendahl-Kaa <github.q2pcy@simplelogin.com>
2022-12-06 15:12:55 +0100

    Hang man

--- Kap 6/Ex 7/sketch.js
+++ Kap 6/Ex 7/sketch.js
@@ -1,9 +1,16 @@
-let word = ["p", "n", "e", "u", "m", "o", "n", "i", "a"];
-let letter, input, button, isCorrect;
+let letter, input, button, isCorrect, gameOver, isDead, correct, word, table;
 let wrong = [];
 let pressed = [];
 
+function preload() {
+  table = loadTable("assets/words_alpha.txt", "txt");
+}
 function setup() {
+  let tableArray = table.getArray();
+  do {
+    word = random(tableArray)[0].toLowerCase();
+    print(word);
+  } while (word.length < 4 || word.length > 8);
   createCanvas(windowWidth, windowHeight);
   background(220);
   textAlign(CENTER);
@@ -22,13 +29,28 @@ function setup() {
 
   button = createButton("Submit");
   button.position(input.x + input.width, input.y);
-  button.mousePressed(SubmitWord);
+  button.mousePressed(submitWord);
+  correct = 0;
+  isDead = 0;
+  gameOver = 0;
+  wrong = [];
+  pressed = [];
+}
+
+function keyPressed() {
+  if (keyCode == 13) {
+    if (gameOver == 0) {
+      submitWord();
+    } else {
+      setup();
+    }
+  }
 }
 
 function drawLetter(i) {
   text(letter.toUpperCase(), 400 + 45 * i + 15, 630);
 }
-function SubmitWord() {
+function submitWord() {
   if (checkIfAlreadyPressed()) {
     return false;
   }
@@ -61,6 +83,8 @@ function checkCorrectLetter() {
     if (letter == wordLetter) {
       drawLetter(i);
       isCorrect = 1;
+      checkWon();
+      print("Correct!");
     }
   }
   if (isCorrect == 0) {
@@ -78,7 +102,12 @@ function checkIfAlreadyPressed() {
     }
   }
 }
-
+function checkWon() {
+  correct++;
+  if (correct == word.length) {
+    gameIsOver();
+  }
+}
 function drawMan(n) {
   // Head
   if (n == 1) {
@@ -93,6 +122,35 @@ function drawMan(n) {
     line(600, 270, 500, 180);
   }
   // Arm 2
+  if (n == 4) {
+    line(600, 270, 700, 180);
+  }
   // Leg 1
+  if (n == 5) {
+    line(600, 400, 500, 490);
+  }
   // Leg 2
+  if (n == 6) {
+    line(600, 400, 700, 490);
+    isDead = 1;
+    gameIsOver();
+  }
+}
+
+function gameIsOver() {
+  if (isDead == 1) {
+    text("YOU DIED!", width / 2 + 300, height / 2);
+    textSize(15);
+    text("Word was:", width / 2 + 300, height / 2 + 70);
+    text(word, width / 2 + 300, height / 2 + 85);
+  } else {
+    text("YOU WON!", width / 2 + 300, height / 2);
+  }
+  textSize(30);
+  text("Press 'ENTER' to reset", width / 2 + 300, height / 2 + 50);
+
+  gameOver = 1;
+  input.hide();
+  button.hide();
+  textSize(100);
 }

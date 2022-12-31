24351c6057e7ea1c8910ec3b6929697b14ddf544
Rasmus Rosendahl-Kaa <github.q2pcy@simplelogin.com>
2022-12-31 12:04:06 +0100

    Snake Game

--- Other stuff/Snake Game/sketch.js
+++ Other stuff/Snake Game/sketch.js
@@ -1,8 +1,8 @@
 let P = 25,
-  speedX = 1,
+  speedX = 2.5,
   speedY = 0,
-  headX,
-  headY;
+  speed = 2,
+  snake = [];
 function setup() {
   createCanvas(
     round((windowWidth - 12.5) / 25) * 25,
@@ -10,8 +10,9 @@ function setup() {
   );
   noStroke();
   ellipseMode(CENTER);
-  headX = round((windowWidth / 2 - 12.5) / 25) * 25;
-  headY = round((windowHeight / 2 - 12.5) / 25) * 25;
+  let headX = round((windowWidth / 2 - 12.5) / 25) * 25;
+  let headY = round((windowHeight / 2 - 12.5) / 25) * 25;
+  snake[0] = new Snakes(headX, headY, speedX, speedY);
 }
 
 function drawBackground() {
@@ -39,29 +40,84 @@ function drawBackground() {
 function draw() {
   drawBackground();
   fill("Blue");
-  rect(headX, headY, 25);
-  headX += speedX;
-  headY += speedY;
+  snake[0].update();
+  snake[0].display();
 }
 
 function keyPressed() {
   if (keyCode == LEFT_ARROW) {
-    speedX = -1;
+    speedX = -2.5;
     speedY = 0;
-    print(headY);
-    headY = round(headY / 25) * 25;
-    print(headY);
   } else if (keyCode == RIGHT_ARROW) {
-    speedX = 1;
+    speedX = 2.5;
     speedY = 0;
-    headY = round(headY / 25) * 25;
   } else if (keyCode == UP_ARROW) {
-    speedY = -1;
+    speedY = -2.5;
     speedX = 0;
-    headX = round(headX / 25) * 25;
   } else if (keyCode == DOWN_ARROW) {
-    speedY = 1;
+    speedY = 2.5;
     speedX = 0;
-    headX = round(headX / 25) * 25;
+  }
+}
+
+function overSideX(x, y) {
+  if (x > width - 25) {
+    let xCoord = x - width;
+    rect(xCoord, y, 25);
+    if (x > width) {
+      print("Completely Over");
+      return xCoord;
+    }
+  } else if (x < 0) {
+    let xCoord = x + width;
+    rect(xCoord, y, 25);
+    if (x < -25) {
+      print("Completely Over");
+      return xCoord;
+    }
+  }
+  return x;
+}
+
+function overSideY(x, y) {
+  if (y > height - 25) {
+    let yCoord = y - height;
+    rect(x, yCoord, 25);
+    if (y > height) {
+      print("Completely Over");
+      return yCoord;
+    }
+  } else if (y < 0) {
+    let yCoord = y + height;
+    rect(x, yCoord, 25);
+    if (y < -25) {
+      print("Completely Over");
+      return yCoord;
+    }
+  }
+  return y;
+}
+
+class Snakes {
+  constructor(xPos, yPos, xVel, yVel) {
+    this.position = new createVector(xPos, yPos);
+    this.velocity = new createVector(xVel, yVel);
+  }
+  update() {
+    if (this.position.y == round(this.position.y / 25) * 25) {
+      this.velocity.x = speedX;
+      this.position.x += this.velocity.x;
+      print(this.velocity, "1");
+    }
+    if (this.position.x == round(this.position.x / 25) * 25) {
+      this.velocity.y = speedY;
+      this.position.y += this.velocity.y;
+      print(this.velocity, "2");
+    }
+    this.position.x = overSideX(this.position.x, this.position.y);
+    this.position.y = overSideY(this.position.x, this.position.y);
+  }
+  display() {
+    rect(this.position.x, this.position.y, 25);
   }
 }
